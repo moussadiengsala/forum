30808c38b462fc4cff3695359f6648493335b994
mousdieng <moizadieng@gmail.com>
2023-10-20 17:12:41 +0000

    set up tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["templates/*.html", "static/*.js"],
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
